8f1e855a6a3df18b024d5498781bbed126d706f3
Nico Ames <nicholaus.l.ames@gmail.com>
2020-08-12 17:31:08 -0500

    No translations

--- src/Field.php
+++ src/Field.php
@@ -28,7 +28,7 @@ class Field extends \craft\base\Field
      */
     public static function displayName(): string
     {
-        return Craft::t('ckeditor', 'CKEditor');
+        return 'CKEditor';
     }
 
     // Properties
